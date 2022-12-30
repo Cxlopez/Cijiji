3cf8d3179f3b5d88ade1ade80660407d76017e8a
Cxlopez <cristian4402@hotmail.com>
2022-12-30 18:45:51 -0500

    add items card to app

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import "../styles/Cards.css"
 
 function Cards() {
   return (
     <div className='card-container'>
       <div className='card'>
         <div className='card-content'>
-
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-content'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-content'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-content'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-content'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-content'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
         </div>
       </div>
     </div>
