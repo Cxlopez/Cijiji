8691c1798c2cf7c841deae32f6a2cbdb14ea6bec
Cxlopez <cristian4402@hotmail.com>
2023-01-05 14:07:11 -0500

    import all page routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import Cards from './components/Cards';
-import Header from './components/Header';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import MyCijiji from './pages/MyCijiji';
+import Post from './pages/Post';
+import Favourites from './pages/Favourites';
+import Messages from './pages/Messages';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Cards />
+
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<MyCijiji />} />
+        <Route path='/' element={<Post />} />
+        <Route path='/' element={<Favourites />} />
+        <Route path='/' element={<Messages />} />
+      </Routes>
     </div>
   );
 }
