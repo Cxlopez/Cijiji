c4301f55d9e4d65a7e803bd603fe9aa1444ce664
Cxlopez <cristian4402@hotmail.com>
2023-01-10 22:24:42 -0500

    adjust styling colour

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import "../styles/Messages.css"
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMessage } from '@fortawesome/free-solid-svg-icons'
 
 function Messages() {
   return (
@@ -7,6 +10,12 @@ function Messages() {
       <nav className='message-nav'>
         <p>My Messages</p>
       </nav>
+      <div className='messages-text'>
+      <FontAwesomeIcon icon={faMessage} className='messages-icon' />
+      <span className='no-message-text'>You have no messages.</span>
+      <span className='want-money'>Want to make some money?</span>
+      <Link to="/post" className='messages-link'>Post an Ad</Link>
+      </div>
     </div>
   )
 }
