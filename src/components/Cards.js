7512b7f619532e384e67b47c081550c1b6103580
Cxlopez <cristian4402@hotmail.com>
2023-01-18 18:38:02 -0500

    add grid styling to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,22 +4,46 @@ import "../styles/Cards.css"
 function Cards() {
   return (
     <div className='card-container'>
-      <div className='row'>
+      <div className='cards'>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
           <h4>lamborghini 10/10</h4>
+          <span>price</span>
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
           <h4>lamborghini 10/10</h4>
+          <span>price</span>
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
           <h4>lamborghini 10/10</h4>
+          <span>price</span>
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
           <h4>lamborghini 10/10</h4>
+          <span>price</span>
+        </div>
+        <div className='card'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+          <h4>lamborghini 10/10</h4>
+          <span>price</span>
+        </div>
+        <div className='card'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+          <h4>lamborghini 10/10</h4>
+          <span>price</span>
+        </div>
+        <div className='card'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+          <h4>lamborghini 10/10</h4>
+          <span>price</span>
+        </div>
+        <div className='card'>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
+          <h4>lamborghini 10/10</h4>
+          <span>price</span>
         </div>
       </div>
     </div>
