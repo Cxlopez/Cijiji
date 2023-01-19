b983ed56f06ce77916e1b4e1df4806a7345dc3e5
Cxlopez <cristian4402@hotmail.com>
2023-01-18 19:33:31 -0500

    add card price and fav-icon

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../styles/Cards.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 function Cards() {
   return (
@@ -7,43 +9,39 @@ function Cards() {
       <div className='cards'>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
+          <p>lamborghini 10/10</p>
           <span>price</span>
+          <FontAwesomeIcon icon={faHeart} className='card-icon' />
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
+          <p>lamborghini 10/10</p>
           <span>price</span>
+          <FontAwesomeIcon icon={faHeart} className='card-icon' />
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
+          <p>lamborghini 10/10</p>
           <span>price</span>
+          <FontAwesomeIcon icon={faHeart} className='card-icon' />
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
+          <p>lamborghini 10/10</p>
           <span>price</span>
+          <FontAwesomeIcon icon={faHeart} className='card-icon' />
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
+          <p>lamborghini 10/10</p>
           <span>price</span>
+          <FontAwesomeIcon icon={faHeart} className='card-icon' />
         </div>
         <div className='card'>
           <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
+          <p>lamborghini 10/10</p>
           <span>price</span>
-        </div>
-        <div className='card'>
-          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
-          <span>price</span>
-        </div>
-        <div className='card'>
-          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/huracan/gallery/gate_hura_03.jpg" alt="car" />
-          <h4>lamborghini 10/10</h4>
-          <span>price</span>
+          <FontAwesomeIcon icon={faHeart} className='card-icon' />
         </div>
       </div>
     </div>
