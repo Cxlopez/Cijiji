edc276ef3143a6a340cdb05ff6755e64dbdacd92
Cxlopez <cristian4402@hotmail.com>
2023-01-24 17:24:13 -0500

    remove magnify glass icon

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 
 function Search() {
   return (
     <div className='search'>
-      {/* <FontAwesomeIcon icon={faMagnifyingGlass} className='search-icon' /> */}
       <input className='search-bar' type="text" placeholder='Search for anything...' />
     </div>
   )
