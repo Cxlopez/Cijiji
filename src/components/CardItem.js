a407559c6cac30732663bb9a9e4cbeeb87dd95e5
Cxlopez <cristian4402@hotmail.com>
2023-03-12 21:40:35 -0400

    adjust cards CSS after creating cardItem

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,20 +1,20 @@
 import { React } from 'react';
-import "../styles/Cards.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import "../styles/CardItem.css"
 
 function CardItem({ad}) {
   return (
-    <div className='card-container'>
-      <div className='cards'>
+
+
         <div className='card'>
           <img src={ad.thumbnail_url} alt="car" />
           <p>{ad.title}</p>
           <span>{ad.price}</span>
           <FontAwesomeIcon icon={faHeart} className='card-icon' />
         </div>
-      </div>
-    </div>
+
+
   )
 }
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,24 +5,26 @@ import "../styles/Cards.css"
 
 
 function Cards() {
-
   const [ads, setAds] = useState([]);
 
   useEffect(() => {
-    axios.get('htp://localhost:8000/api/ads')
+    axios.get('http://localhost:8000/api/ads')
       .then((res) => {
-        setAds(res.data.ads)
+        setAds(res.data.ads);
+
       })
-  })
+  }, [])
 
 
   return (
-    <div>
+    <div className='card-container'>
+    <div className='cards'>
       {
         ads.map(ad => (
           <CardItem key={ad.id} ad={ad} />
         ))
       }
+    </div>
     </div>
   )
 }
